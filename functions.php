9858825cab0edf6774cf11ebf0d8e5d70c631639
SvenK <sven@vortige.com>
2026-05-03 19:36:59 +0200

    Enqueue child theme stylesheet after parent stylesheet

--- functions.php
+++ functions.php
@@ -1,7 +1,19 @@
 <?php defined( 'ABSPATH' ) || exit;
 
 add_action( 'wp_enqueue_scripts', function () {
-	
-	wp_enqueue_style( 'jkd-parent-style', get_template_directory_uri() . '/style.css' );
-	
-} );
+
+    // Parent-CSS laden
+    wp_enqueue_style(
+        'jkd-parent-style',
+        get_template_directory_uri() . '/style.css'
+    );
+
+    // Child-CSS nach dem Parent-CSS laden
+    wp_enqueue_style(
+        'jkd-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        ['jkd-parent-style'],
+        wp_get_theme()->get( 'Version' )
+    );
+
+}, 20);
